1f8d3e7ecee3ad786bcf7a92187a7786aa456007
João Victor <joaovitor.lopez662@gmail.com>
2022-12-15 15:55:56 -0400

    entrega s3-14 1.0

---
+++ script.js
@@ -0,0 +1,66 @@
+//atividade 1
+// let gasolina = 6.8;
+// let pago = parseFloat(prompt(`quanto dinheiro você quer gastar?`));
+// let saida = (pago / gasolina).toFixed(2) + " L";
+// console.log(`o cliente abasteceu ${saida}`);
+
+//atividade 2
+// let c = parseInt(prompt(`qual a temperatura do ambiente em C?`));
+// let fa = (9 * c + 160) / 5;
+// console.log(`o valor de ${c} em Fahrenheit é ${fa}`);
+
+//atividade 3
+// let fe = parseFloat(prompt(`qual a temperatura do ambiente em F?`));71.6
+// let c = (((fe - 32) * 5) / 9).toFixed(0);
+// console.log(`o valor ${fe} em Celsius é ${c}`);
+
+//atividade 4
+// let comprimento = parseInt(prompt(`qual o comprimento do caixa?`));
+// let largura = parseInt(prompt(`qual a largura da caixa?`));
+// let altura = parseInt(prompt(`qual a altura da caixa?`));
+// let volume = comprimento * largura * altura;
+// console.log(`O volume é de: ${volume}m³`);
+
+//atividade 5
+// let numero = parseInt(prompt(`digite um número`));
+// let elevado = numero ** 2;
+// alert(`o valor de ${numero} ao quadrado é ${elevado}`);
+
+//atividade 6
+// let numero1 = parseInt(prompt(`digite o primeiro número`));
+// let numero2 = parseInt(prompt(`digite o segundo número`));
+// let numero3 = parseInt(prompt(`digite o treceiro número`));
+// let numero4 = parseInt(prompt(`digite o quarto número`));
+// let numero5 = parseInt(prompt(`digite o quinto número`));
+// let soma = numero1 + numero2 + numero3 + numero4 + numero5;
+// let media = soma / 5;
+// console.log(
+//   `A soma dos valores informados é: ${soma} e a média das notas é:${media}`
+// );
+
+//atividade 7
+// let numero1 = parseInt(prompt(`digite um número`));
+// let numero2 = parseInt(prompt(`digite mais um número`));
+// let sobra = numero1 % numero2;
+// console.log(`o resto da divisão de ${numero1} por ${numero2} é:${sobra}`);
+
+//atividade 8
+// let palavra = `pneumoultramicroscopicossilicovulcanoconiótico`.length;
+// console.log(`este texto tem ${palavra} caracteres`);
+
+//atividade 9
+// let nome = `pneumoultramicroscopicossilicovulcanoconiótico`;
+// let primeiraLetra = nome.slice(0, 1);
+// let ultimaLetra = nome.slice(-1);
+// console.log(
+//   `a primeira letra desde texto é ${primeiraLetra.toUpperCase()} e a última é ${ultimaLetra}`
+// );
+
+// atividade 10
+// let nome = "geraldo";
+// let sobrenome = "luis";
+// let idade = 32;
+// let setor = "Produção";
+// console.log(
+//   `nome:${nome.toUpperCase()} ${sobrenome.toUpperCase()}-idade:${idade}-Setor:${setor}`
+// );
